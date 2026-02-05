73f4da62ce83b13fdbe5112f78f2214e2a69a16b
Sultan2403 <sultanremilekun@gmail.com>
2026-02-05 17:07:59 +0100

    implemented user login endpoint

--- Backend/Controllers/users.controller.js
+++ Backend/Controllers/users.controller.js
@@ -1,6 +1,8 @@
 const usersCollection = require("../DB/Models/users.model");
 const bcrypt = require("bcryptjs");
 
+console.log(require("crypto").randomBytes(64).toString("hex"));
+
 const registerUser = async (req, res) => {
   try {
     const { password, ...data } = req.body;
@@ -16,4 +18,36 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const { password, email } = req.body;
+    const existingUser = await usersCollection.findOne({ email });
+
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    console.log(isMatch)
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: existingUser._id, email },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN || "1h" },
+    );
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.error(err, err.message);
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+};
+
+module.exports = { registerUser, loginUser };

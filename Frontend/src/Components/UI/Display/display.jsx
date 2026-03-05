4055f9a051847766956da1bb60974d6013aae8ea
Loner <sultanremilekun@gmail.com>
2026-03-05 16:57:20 +0100

    Update display.jsxfixed stuff that broke prod

--- Frontend/src/Components/UI/Display/display.jsx
+++ Frontend/src/Components/UI/Display/display.jsx
@@ -67,7 +67,7 @@ export default function Display() {
   return (
     <div className="px-10 py-8 bg-offwhite min-h-[400px] rounded-lg shadow-soft">
       <h2 className="text-2xl font-serif font-medium text-softBrown mb-6">
-        Welcome back, {userData.username || "Guest"}
+        Welcome back, {userData?.username || "Guest"}
       </h2>
       <h2 className="text-2xl font-serif font-medium text-softBrown mb-6">
         Never lose a thought again

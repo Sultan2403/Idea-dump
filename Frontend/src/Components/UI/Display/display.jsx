55cf559e8132799ab480a59df10b6be8dbbe1174
Sultan2403 <sultanremilekun@gmail.com>
2026-03-06 19:55:58 +0100

    improved welcome message

--- Frontend/src/Components/UI/Display/display.jsx
+++ Frontend/src/Components/UI/Display/display.jsx
@@ -67,7 +67,9 @@ export default function Display() {
   return (
     <div className="px-10 py-8 bg-offwhite min-h-[400px] rounded-lg shadow-soft">
       <h2 className="text-2xl font-serif font-medium text-softBrown mb-6">
-        Welcome back, {userData?.username || "Guest"}
+        {userData?.username
+          ? `Welcome back, ${userData?.username}`
+          : "Welcome Guest"}
       </h2>
       <h2 className="text-2xl font-serif font-medium text-softBrown mb-6">
         Never lose a thought again

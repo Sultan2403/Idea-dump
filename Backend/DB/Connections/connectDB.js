bb7d3f2793afc2385b7d10b3926bd248d09ae47b
Sultan2403 <sultanremilekun@gmail.com>
2026-03-06 22:03:47 +0100

    changed URL to URI in mongodb connection

--- Backend/DB/Connections/connectDB.js
+++ Backend/DB/Connections/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URL);
+    await mongoose.connect(process.env.MONGO_DB_URI);
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error);

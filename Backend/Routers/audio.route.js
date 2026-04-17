7bfac37a16e81831c3ea84b934158d3a62091bc8
Sultan2403 <sultanremilekun@gmail.com>
2026-04-17 20:59:52 +0100

    rename speech-to-text files to audio

--- Backend/Routers/audio.route.js
+++ Backend/Routers/audio.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const speech_to_text_controller = require("../Controllers/speech-to-text.controller");
+const speech_to_text_controller = require("../Controllers/audio.controller");
 
 router.post("/", speech_to_text_controller);
 
